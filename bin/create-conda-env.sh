e347db2db3ba858a98b065be3db8d17b38c280a8
David R. Pugh <drpugh@protonmail.com>
2021-03-02 08:18:44 +0300

    Missing /

--- bin/create-conda-env.sh
+++ bin/create-conda-env.sh
@@ -3,4 +3,4 @@
 # create the conda environment
 PROJECT_DIR=$PWD
 ENV_PREFIX=PROJECT_DIR/env
-conda env create --prefix $ENV_PREFIX --file PROJECT_DIRenvironment.yml --force
+conda env create --prefix $ENV_PREFIX --file PROJECT_DIR/environment.yml --force
