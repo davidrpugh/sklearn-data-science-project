9bafc582768364adc56bd950a5f0d82982fa5bb9
ibex-training <ibex-training@kaust.edu.sa>
2021-03-07 10:39:02 +0300

    First draft of train.py and train.sbatch scripts.

---
+++ src/train.py
@@ -0,0 +1,55 @@
+import joblib
+import numpy as np
+import pandas as pd
+from sklearn import datasets, decomposition, ensemble, metrics, model_selection, pipeline, preprocessing
+
+
+DATASET_NAME = "CIFAR_10"
+N_COMPONENTS = 0.95
+SEED = 42
+TEST_SIZE = 0.1
+TRAIN_N_JOBS = -1
+VERBOSITY = 10
+
+
+# download the dataset
+bunch = datasets.fetch_openml(name=DATASET_NAME, as_frame=True)
+data_df, target_df = bunch["data"], bunch["target"]
+
+# split the dataset into training and testing data
+_random_state = (np.random
+                   .RandomState(SEED))
+train_df, test_df, train_target, test_target = model_selection.train_test_split(
+    data_df,
+    target_df,
+    test_size=TEST_SIZE,
+    random_state=_random_state
+)
+
+# create a pipeline
+ml_pipeline = pipeline.make_pipeline(
+    preprocessing.MinMaxScaler(),
+    decomposition.PCA(n_components=N_COMPONENTS, random_state=_random_state),
+    ensemble.RandomForestClassifier(n_jobs=TRAIN_N_JOBS, random_state=_random_state, verbose=VERBOSITY),
+    verbose=True,
+)
+
+# fit the pipeline and save the trained model to disk
+_ = ml_pipeline.fit(train_df, train_target)
+joblib.dump(ml_pipeline, f"../results/ml-pipeline.joblib")
+
+# make predictions
+predictions = ml_pipeline.predict(test_df)
+
+# generate a classification report
+classification_report = metrics.classification_report(
+    test_target,
+    predictions,
+)
+print(classification_report)
+
+
+
+
+
+
