dae5a29d0514d7fe262d7809e320d406eba56736
pughdr <david.pugh@kaust.edu.sa>
2021-03-10 07:18:13 +0300

    Quoting variables to protect against ws in user directories

--- bin/launch-train.sh
+++ bin/launch-train.sh
@@ -4,12 +4,12 @@
 set -e
 
 # script should be run from the project directory
-export PROJECT_DIR=$PWD
+export PROJECT_DIR="$PWD"
 
 # creates a separate directory for each job
 JOB_NAME=example-training-job
-mkdir -p $PROJECT_DIR/results/$JOB_NAME
+mkdir -p "$PROJECT_DIR"/results/"$JOB_NAME"
 
 # launch the training job
-sbatch --job-name $JOB_NAME $PROJECT_DIR/bin/train.sbatch $PROJECT_DIR/src/train.py 
+sbatch --job-name "$JOB_NAME" "$PROJECT_DIR"/bin/train.sbatch "$PROJECT_DIR"/src/train.py 
 
