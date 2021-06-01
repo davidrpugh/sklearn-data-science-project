6a236b7bd1d7730efba47a315f7bdc8ac9bb6295
pughdr <david.pugh@kaust.edu.sa>
2021-06-01 09:29:01 +0300

    Prefer mamba over Conda for this environment.

--- bin/create-conda-env.sh
+++ bin/create-conda-env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash --login
 
 # create the conda environment
-PROJECT_DIR=$PWD
-ENV_PREFIX=$PROJECT_DIR/env
-conda env create --prefix $ENV_PREFIX --file $PROJECT_DIR/environment.yml --force
+PROJECT_DIR="$PWD"
+ENV_PREFIX="$PROJECT_DIR"/env
+mamba env create --prefix $ENV_PREFIX --file "$PROJECT_DIR"/environment.yml --force
